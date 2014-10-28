68b61645dc4ac1ed8f9d5401a205e34cd2a7e058
Aditya Narayan <narayanaditya95@gmail.com>
2014-10-29 02:55:43 +0530

    removed pipeline'

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -69,20 +69,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-SOCIAL_AUTH_PIPELINE = (
-'social.pipeline.social_auth.social_details',
-'social.pipeline.social_auth.social_uid',
-'social.pipeline.social_auth.auth_allowed',
-'social_auth.backends.pipeline.social.social_auth_user',
-'social_auth.backends.pipeline.associate.associate_by_email',
-'social_auth.backends.pipeline.misc.save_status_to_session',
-'social_auth.backends.pipeline.user.create_user',
-'social_auth.backends.pipeline.social.associate_user',
-'social_auth.backends.pipeline.social.load_extra_data',
-'social_auth.backends.pipeline.user.update_user_details',
-'social_auth.backends.pipeline.misc.save_status_to_session',
-)
-
 LOGIN_URL          = '/landingPage/'
 LOGIN_REDIRECT_URL = '/landingPage/'
 LOGIN_ERROR_URL    = '/'

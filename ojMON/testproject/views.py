a3b3655bd20b668db7aa96e9d20b63202bb476cd
Aditya Narayan <narayanaditya95@gmail.com>
2014-10-29 02:26:45 +0530

    tried correcting token error

--- ojMON/testproject/views.py
+++ ojMON/testproject/views.py
@@ -150,13 +150,3 @@ def dummy_tester(num):
         if m.stdout==i.solution:
             return True
     return False
-
-def save_profile(backend, user, response, *args, **kwargs):
-    if backend.name == 'facebook':
-        profile = user.get_profile()
-        if profile is None:
-            profile = Profile(user_id=user.id)
-        profile.gender = response.get('gender')
-        profile.link = response.get('link')
-        profile.timezone = response.get('timezone')
-        profile.save()

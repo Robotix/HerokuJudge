e6f6b96146c615103013e001d6436bab01fc3902
Aditya Narayan <narayanaditya95@gmail.com>
2014-10-29 01:29:13 +0530

    finally

--- solutions/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from solutions.models import sol
-
-admin.site.register(sol)
--- testResults/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from testResults.models import testResults
-
-admin.site.register(testResults)

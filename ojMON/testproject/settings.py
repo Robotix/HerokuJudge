30cebe672b6d1a2f3109d1a4cf66239528bdfeca
Aditya Narayan <narayanaditya95@gmail.com>
2014-10-29 02:24:03 +0530

    tried correcting token error

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -73,8 +73,6 @@ SOCIAL_AUTH_PIPELINE = (
 'social_auth.backends.pipeline.social.social_auth_user',
 'social_auth.backends.pipeline.associate.associate_by_email',
 'social_auth.backends.pipeline.misc.save_status_to_session',
-'app.pipeline.redirect_to_form',
-'app.pipeline.username',
 'social_auth.backends.pipeline.user.create_user',
 'social_auth.backends.pipeline.social.associate_user',
 'social_auth.backends.pipeline.social.load_extra_data',

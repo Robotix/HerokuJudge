65d3a7236b062736b102b2187a4b07f840fce5cc
Aditya Narayan <narayanaditya95@gmail.com>
2014-10-29 05:50:39 +0530

    added github

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -72,6 +72,7 @@ AUTHENTICATION_BACKENDS = (
    'social.backends.facebook.FacebookOAuth2',
    'social.backends.google.GoogleOAuth2',
    'social.backends.twitter.TwitterOAuth',
+   'social.backends.github.GithubOAuth2',
    'django.contrib.auth.backends.ModelBackend',
 )
 

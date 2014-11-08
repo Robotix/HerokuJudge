68b151e94acf48932c3b2e032624d11c98eab253
Aditya Narayan <narayanaditya95@gmail.com>
2014-11-08 21:28:50 +0530

    changed colour

--- ojMON/testResults/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class testResults(models.Model):
-    solution = models.TextField()
-    prob = models.IntegerField()
--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'solutions',
-    'testResults',
     'social.apps.django_app.default',
 )
 

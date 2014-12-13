1a55dffa1f2d2fb9a8a0d6b56e2380872ebd19a5
Aditya Narayan <narayanaditya95@gmail.com>
2014-12-13 06:28:28 +0530

    changed debug to false

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'z6ex3!ov!vq4c$l$tn%k9x^&_va2(w$a+pe-(60+bg93y0$^u$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 TEMPLATE_DIRS = (
     BASE_DIR+'/templates/',

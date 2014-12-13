9177d0e518af18cef459ff25b69aadd777b81259
Aditya Narayan <narayanaditya95@gmail.com>
2014-12-13 06:01:33 +0530

    changed db to aws

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -129,18 +129,18 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'da4abl57es557h',
-#         'USER': 'xeujnnexbxwkde',
-#         'PASSWORD': 'dEieJrTwnUA9zsNSdfRDoUmBnh',
-#         'HOST': 'ec2-54-204-40-140.compute-1.amazonaws.com',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'da4abl57es557h',
+        'USER': 'xeujnnexbxwkde',
+        'PASSWORD': 'dEieJrTwnUA9zsNSdfRDoUmBnh',
+        'HOST': 'ec2-54-204-40-140.compute-1.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
-DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': 'postgres', 'USER': 'robotix2014', 'PASSWORD': 'robotix2014', 'HOST': 'localhost', 'PORT': '5432' } }
+# DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': 'postgres', 'USER': 'robotix2014', 'PASSWORD': 'robotix2014', 'HOST': 'localhost', 'PORT': '5432' } }
 
 # Internationalization
 # https://docs.djan,goproject.com/en/1.6/topics/i18n/

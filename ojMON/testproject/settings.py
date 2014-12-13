340bd771e66844f27361c139904036ba466c8dd6
Aditya Narayan <narayanaditya95@gmail.com>
2014-12-13 06:33:45 +0530

    changed template debug to true

--- ojMON/testproject/settings.py
+++ ojMON/testproject/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'z6ex3!ov!vq4c$l$tn%k9x^&_va2(w$a+pe-(60+bg93y0$^u$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

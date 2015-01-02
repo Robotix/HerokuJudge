b1b3a00fcb4c2cb9a474a31a04ccc35a8483d7bf
Aditya Narayan <narayanaditya95@gmail.com>
2015-01-02 11:16:27 +0530

    fixed static files from apps

--- sudocode/settings.py
+++ sudocode/settings.py
@@ -90,7 +90,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 )
 
 ################################# Python Social Auth Settings #################################
